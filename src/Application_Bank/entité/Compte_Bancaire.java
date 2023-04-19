b8728944ff7477ca3c27e7b64d17b90e09b52569
kouaouci <karimakouaouci2803@gmail.com>
2023-04-19 11:44:44 +0200

    service compte bancaire terminé

--- src/Application_Bank/entité/Compte_Bancaire.java
+++ src/Application_Bank/entité/Compte_Bancaire.java
@@ -2,6 +2,22 @@ package Application_Bank.entité;
 
 public class Compte_Bancaire {
     private  int numéroDeCompte;
+
+    public Compte_Bancaire() {
+        this.numéroDeCompte=(int)Math.random ()*100000;
+        this.montant=0;
+    }
+
+    public Compte_Bancaire(double montant) {
+        this.numéroDeCompte=(int)Math.random ()*100000;
+        this.montant = montant;
+    }
+
+    public Compte_Bancaire(int numéroDeCompte, double montant) {
+        this.numéroDeCompte = numéroDeCompte;
+        this.montant = montant;
+    }
+
     private double montant;
 
     public int getNuméroDeCompte() {
@@ -16,3 +32,4 @@ public class Compte_Bancaire {
         this.montant = montant;
     }
 }
+
--- src/Application_Bank/service/CompteService.java
+++ src/Application_Bank/service/CompteService.java
@@ -1,4 +1,38 @@
 package Application_Bank.service;
 
+import Application_Bank.entité.Compte_Bancaire;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CompteService {
+    public void virement(Compte_Bancaire comptecourant, Compte_Bancaire compteepargne, double montant){
+        if(comptecourant.getMontant ()<montant){
+            System.out.println ("Vous avez pas assez d'argent");
+
+        }else{
+            comptecourant.setMontant ( comptecourant.getMontant ()-montant );
+            compteepargne.setMontant ( compteepargne.getMontant ()+montant );
+            System.out.println (String.valueOf ( montant )+"virement du compte"+ comptecourant.getNuméroDeCompte ()+ "au compte"+ compteepargne.getNuméroDeCompte ());
+
+        }
+
+    }
+
+    public ArrayList<Compte_Bancaire>getAllCompte() throws FileNotFoundException{
+        ArrayList<Compte_Bancaire> compte_bancaires=new ArrayList<Compte_Bancaire> ();
+        Scanner sc = new Scanner ( new File ( "" ) );
+        sc.useDelimiter ( "\\r" );
+        while (sc.hasNext ()){
+            String dataLine= sc.next ();
+            String[] data= dataLine.split ( ";",2 );
+            compte_bancaires.add ( new Compte_Bancaire (Integer.valueOf ( data[0].trim () ),Double.parseDouble ( data[1].trim () )) );
+
+        }
+        sc.close ();
+        return compte_bancaires;
+    }
+
 }
